605f39f4eb9a015cfeda1c0abb269cf25116f3b4
David Yaari <david,yaari@icloud.com>
2022-06-12 15:41:15 +0300

    added navigation and firebase packages

--- readme.md
+++ readme.md
@@ -7,3 +7,13 @@ npx react-native init goldrushrun --template react-native-template-typescript
 `
 
 ## Update the bundle name and create the src directory structure
+
+## Add the navigation
+
+`
+npm install @react-navigation/native
+npm install @react-navigation/native-stack
+`
+
+## Add Firebase (select web), add constants
+npm install firebase
---
+++ src/constants/firebase.ts
@@ -0,0 +1,15 @@
+import firebase from 'firebase/compat/app';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyCRWHZ8ohsFvMQamLxuocCoqRkUq1nAD80',
+  authDomain: 'goldrushrun2022.firebaseapp.com',
+  projectId: 'goldrushrun2022',
+  storageBucket: 'goldrushrun2022.appspot.com',
+  messagingSenderId: '153391078320',
+  appId: '1:153391078320:web:0db184de3caa32c99c1475',
+  measurementId: 'G-PLQ3F4KS9H',
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
